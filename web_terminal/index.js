e144e22d3700a54cacf19cf7a43ed74825adb6a3
satoshi <satoshiho2012@gmail.com>
2023-04-06 14:17:53 +0800

    update

--- web_terminal/index.js
+++ web_terminal/index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { exec } = require('child_process');
+const { spawn } = require('child_process');
 const http = require('http');
 const socketIO = require('socket.io');
 const app = express();
@@ -16,19 +16,42 @@ app.get('/', (req, res) => {
 io.on('connection', (socket) => {
   console.log('A user connected');
 
-  socket.on('command', (command) => {
-    exec(command, (error, stdout, stderr) => {
-      if (error) {
-        console.error(`exec error: ${error}`);
-        return socket.emit('output', `Error executing command: ${error.message}`);
-      }
+  // Spawn a new shell instance for each connected user
+  const shell = spawn(process.platform === 'win32' ? 'cmd' : 'bash');
 
-      socket.emit('output', stdout || stderr);
-    });
+  const emitBufferedLines = (buffer, emitFunction) => {
+    const lines = buffer.split('\n');
+    const lastLine = lines.pop();
+    lines.forEach((line) => emitFunction(line));
+    return lastLine;
+  };
+
+  const handleData = (initialBuffer, data, emitFunction) => {
+    const buffer = initialBuffer + data.toString();
+    return emitBufferedLines(buffer, emitFunction);
+  };
+
+  let stdoutBuffer = '';
+  let stderrBuffer = '';
+
+  // Listen for command output and send it back to the user
+  shell.stdout.on('data', (data) => {
+    stdoutBuffer = handleData(stdoutBuffer, data, (line) => socket.emit('output', line));
   });
 
+  shell.stderr.on('data', (data) => {
+    stderrBuffer = handleData(stderrBuffer, data, (line) => socket.emit('output', line));
+  });
+
+  // Listen for command input from the user and send it to the shell
+  socket.on('command', (command) => {
+    shell.stdin.write(command + '\n');
+  });
+
+  // Clean up the shell instance when the user disconnects
   socket.on('disconnect', () => {
     console.log('A user disconnected');
+    shell.kill();
   });
 });
 
